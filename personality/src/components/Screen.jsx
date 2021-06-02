4c16dc25659df027f7bd2a42d7c0a73b448961b8
Thomas Dietrich <tomriiich@gmail.com>
2021-06-02 10:20:50 +0200

    Component creation
    Screen creation
    ESLint installed
    js renamed to jsx
    Hello world test

---
+++ personality/src/components/Screen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Screen() {
+  return (
+    <>
+      <div className="bubble">
+        <p>Hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default Screen;
